adbfc0ad0e3322616e153397aee713e801ff6eed
sherzod02 <kholmamatovsh09>
2022-03-03 00:36:58 +0500

    added up to javascript

--- main.js
+++ main.js
@@ -9,3 +9,32 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 1000);
   }, 1500);
 });
+
+const up = document.querySelector(".up");
+
+var debounce = (func, wait, immediate) => {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const checkScroll = () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 200) {
+    up.classList.add("active");
+  } else {
+    up.classList.remove("active");
+  }
+};
+
+window.addEventListener("scroll", debounce(checkScroll, 20));
